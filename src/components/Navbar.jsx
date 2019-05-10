a5a1c37eafc20be3ec96baa72e95f442274bf083
Kenny Halim <kennyhalim98@gmail.com>
2019-05-10 12:09:10 -0700

    add styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,44 @@ export default function Navbar() {
         .navbar {
           background: lightblue;
           height: 10vh;
-          padding: 10px;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+        }
+
+        .logo {
+          height: 10vh;
+          padding-left: 20px;
           display: flex;
           align-items: center;
+          justify-content: flex-start;
         }
+
         .rightSide {
+          padding-top: 5px;
+          height: 10vh;
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          padding-right: 20px;
         }
+
         .child {
           display: inline-block;
           margin: 20px;
         }
+
+        .child p {
+          border-bottom: 2px solid transparent;
+          padding-bottom: 10px;
+        }
+        .child p:hover {
+          border-bottom: 2px solid black;
+          padding-bottom: 10px;
+          cursor: pointer;
+        }
       `}</style>
       <div className="navbar">
+        <div className="logo">asd</div>
         <div className="rightSide">
           <div className="child">
             <p>Home</p>
